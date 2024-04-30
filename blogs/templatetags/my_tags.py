28d339923d347a7ff83076915886b4586600071f
OleynikovVitaliy <Oleinikov.Vitalik84@gmail.com>
2024-04-30 21:42:17 +0500

    обновлен файл README.md

--- blogs/templatetags/my_tags.py
+++ blogs/templatetags/my_tags.py
@@ -11,14 +11,6 @@ def my_media(val):
     return '#'
 
 
-# @register.filter()
-# def mymedia(val):
-#     if val:
-#         return f'/media/{val}'
-#
-#     return '#'
-
-
 @register.simple_tag()
 def my_media(val):
     if val:
